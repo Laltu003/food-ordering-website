3376b373d7a240b2e0a599e722c27c47d1e6a865
Laltu003 <gorailaltu65@gmail.com>
2026-02-11 13:31:32 +0530

    Add database connection setup in connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,1 +1,13 @@
+<?php
+$servername = "sql100.byetcluster.com";   // Your DB Host
+$username   = "epiz_xxxxxx";              // Your DB Username
+$password   = "YOUR_DB_PASSWORD";         // Your DB Password
+$dbname     = "epiz_xxxxxx_food_db";      // Your DB Name
 
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
